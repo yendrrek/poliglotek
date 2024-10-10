0b3519e8fa4346857e8f105699597120d0d2f8b8
Andrzej Piontek <yendrrek@protonmail.com>
2024-10-11 00:38:28 +0100

    Re-add files missing after dropping commits

---
+++ src/main/java/com/transtopolish/HttpClient.java
@@ -0,0 +1,20 @@
+package com.transtopolish;
+
+import com.transtopolish.models.SearchResponseWrapper;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Header;
+import io.micronaut.http.annotation.QueryValue;
+import io.micronaut.http.client.annotation.Client;
+
+import static io.micronaut.http.HttpHeaders.USER_AGENT;
+
+@Client("https://www.googleapis.com/customsearch/v1")
+@Header(name = USER_AGENT, value = "Micronaut HTTP Client")
+public interface HttpClient {
+    @Get(value = "?key={customSearchApiKey}&cx={searchEngineId}&q='{query}'&lr=lang_es&count=10&safe=active&cr=countryAR",
+            produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON)
+    SearchResponseWrapper fetchSearchResults(@QueryValue String customSearchApiKey,
+                                             @QueryValue String searchEngineId,
+                                             @QueryValue String query);
+}
---
+++ src/main/java/com/transtopolish/config/GoogleCloudConfig.java
@@ -0,0 +1,14 @@
+package com.transtopolish.config;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ConfigurationProperties("googleCloud")
+public class GoogleCloudConfig {
+    private String projectId;
+    private String customSearchApiKey;
+    private String customSearchEngineId;
+}
