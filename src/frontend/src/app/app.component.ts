db4729782e72c829cd826e964228a7eaceba4ead
Andrzej Piontek <yendrrek@protonmail.com>
2024-10-28 22:17:36 +0000

    Add 6 Material Tabs

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {MatTab, MatTabGroup} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatTab, MatTabGroup],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
