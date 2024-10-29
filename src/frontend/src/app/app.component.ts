da020f32a9aa3e99d9ab27374de3c23c0db2e42a
Andrzej Piontek <yendrrek@protonmail.com>
2024-10-29 13:23:45 +0000

    Update front-end
    - Added input for search term
    - Added select drop-downs for the target language and country
    - Increase the number of tabs with search results to 10

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {MatTab, MatTabGroup} from '@angular/material/tabs';
+import {MatFormField, MatLabel, MatSuffix} from '@angular/material/form-field';
+import {MatOption, MatSelect} from '@angular/material/select';
+import {MatInput} from '@angular/material/input';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatTab, MatTabGroup],
+  imports: [RouterOutlet, MatTab, MatTabGroup, MatFormField, MatLabel, MatSelect, MatOption, MatInput, MatSuffix, MatIcon, MatIconButton, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
