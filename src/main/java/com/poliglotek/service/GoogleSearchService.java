43216872f13269c56e509f5422d0794277ca865f
Andrzej Piontek <yendrrek@protonmail.com>
2024-12-29 14:39:51 +0000

    Move `excluded terms` config from application.yml to static class
    - For better separation of concerns and maintenance Google
    Custom Search`excluded terms` configuration was moved from
    application.yml to a static class ExcludedEcommerceTerms.java.
    - Updated GoogleSearchService to reflect that change.
    - Removed obsolete code.

--- src/main/java/com/poliglotek/service/GoogleSearchService.java
+++ src/main/java/com/poliglotek/service/GoogleSearchService.java
@@ -1,35 +1,32 @@
 package com.poliglotek.service;
 
-import com.poliglotek.configuration.googlecustomsearch.ExcludedEcommerceConfig;
+import com.poliglotek.config.ExcludedEcommerceTerms;
 import com.poliglotek.model.googlesearch.SearchItem;
 import com.poliglotek.model.googlesearch.SearchResponseWrapper;
 import io.micronaut.context.annotation.Value;
 import jakarta.inject.Singleton;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
+// https://programmablesearchengine.google.com/controlpanel/overview?cx=8296b888e31bc4fb4
+// Limit of 10,000 queries per day
+// All Amazon domains are exluded through the front-end settings (link above)
 @Singleton
 public class GoogleSearchService {
 
-    private final Logger log = LoggerFactory.getLogger(GoogleSearchService.class);
     private final GoogleCustomSearchClient httpClient;
     private final String customSearchApiKey;
     private final String customSearchEngineId;
-    private final ExcludedEcommerceConfig excludedEcommerceConfig;
     private final String excludedSocialMedia;
     private static final String LANG_PREFIX = "lang_";
 
     public GoogleSearchService(GoogleCustomSearchClient httpClient,
                                @Value("${googleCloud.customSearchApiKey}") String customSearchApiKey,
                                @Value("${googleCloud.customSearchEngineId}")  String customSearchEngineId,
-                               @Value("${excludedSocialMedia}") String excludedSocialMedia,
-                               ExcludedEcommerceConfig excludedEcommerceConfig) {
+                               @Value("${excludedSocialMedia}") String excludedSocialMedia) {
         this.httpClient = httpClient;
         this.customSearchApiKey = customSearchApiKey;
         this.customSearchEngineId = customSearchEngineId;
-        this.excludedEcommerceConfig = excludedEcommerceConfig;
         this.excludedSocialMedia = excludedSocialMedia;
     }
 
@@ -55,7 +52,7 @@ public class GoogleSearchService {
     }
 
     private String buildTermsExcludedFromSearch(String langCode) {
-        String eCommerceTerms = excludedEcommerceConfig.getLanguage().get(langCode.toLowerCase());
+        String eCommerceTerms = ExcludedEcommerceTerms.LANGUAGES.get(langCode.toLowerCase());
         return eCommerceTerms + ", " + excludedSocialMedia;
     }
 }
