d99a2e44def8642a425a6d4b5d0b3499b1f8d7ea
Andrzej Piontek <yendrrek@protonmail.com>
2025-03-15 23:26:23 +0000

    Add Google Sign-in, update podman-build script
    - Added Sign-in button. Currently tested in localhost (WIP).
    - Added Micronaut configuration for securing the application with
    Google (WIP).
    -- Added necessary dependencies
    (oauth2 and jwt from micronaut.security).
    - Created application-npe.yml.
    - Updated Angular environment files (dev and prod) with sign-in url.
    - Updated `podman-build` script to allow creating an image for the npe
    environment.

--- src/frontend/src/environments/environment.prod.ts
+++ src/frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: "https://poliglotek.com",
+  loginUrl: 'https://npe.poliglotek.com/home'
 };
--- src/frontend/src/environments/environment.ts
+++ src/frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   baseUrl: "http://localhost:8080",
+  loginUrl: 'http://localhost:4200'
 };
